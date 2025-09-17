644c206fc0f1aa801d94807e879d00a0aa64be92
Luis Olarte <l.olartec@uniandes.edu.co>
2025-09-17 06:52:31 -0500

    primero

--- src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/PlanetaEntity.java
+++ src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/PlanetaEntity.java
@@ -1,5 +1,9 @@
 package co.edu.uniandes.dse.ParcialPractico1_202520.entities;
+import lombok.Data;
 
+@Data
 public class PlanetaEntity {
     private String nombre;
+    private double poblacion;
+    private double diametroEnKM;
 }
--- src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/SistemaSolar.java
+++ src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/SistemaSolar.java
@@ -1,5 +1,7 @@
 package co.edu.uniandes.dse.ParcialPractico1_202520.entities;
+import lombok.Data;
 
+@Data
 public class SistemaSolar extends BaseEntity {
 
     private String nombre;
